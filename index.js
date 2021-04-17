858e58017e71edb6ca48152101404054bc63d31b
Sarman7105 <sarman7105@gmail.com>
2021-04-17 23:37:42 +0600

    service review and admin implemented

--- index.js
+++ index.js
@@ -18,11 +18,63 @@ app.get('/', (req, res) => {
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0q0ko.mongodb.net/${process.env
 	.DB_NAME}?retryWrites=true&w=majority`;
-console.log(uri);
+// console.log(uri);
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect((err) => {
-    console.log('error',err );
+    // console.log('error',err );
+
+    const serviceCollection = client.db('eventMaker').collection('services');
+    const reviewCollection = client.db('eventMaker').collection('reviews');
+    const adminCollection = client.db('eventMaker').collection('admins');
+
+    app.post('/addService', (req, res) => {
+		const newService = req.body;
+		  console.log('adding new event: ', newService)
+		    serviceCollection.insertOne(newService).then((result) => {
+			console.log('inserted count', result.insertedCount);
+			res.send(result.insertedCount > 0);
+		});
+    });
+    
+
+    app.post('/addReview', (req, res) => {
+		const newReview = req.body;
+		  console.log('adding new review: ', newReview)
+		    reviewCollection.insertOne(newReview).then((result) => {
+			console.log('inserted count', result.insertedCount);
+			res.send(result.insertedCount > 0);
+		});
+    });
+
+    app.post('/addAdmin', (req, res) => {
+		const newAdmin = req.body;
+		  console.log('adding new review: ', newAdmin)
+		    adminCollection.insertOne(newAdmin).then((result) => {
+			console.log('inserted count', result.insertedCount);
+			res.send(result.insertedCount > 0);
+		});
+    });
+    
+    app.get('/services', (req, res) => {
+		serviceCollection.find().toArray((err, items) => {
+			res.send(items);
+		});
+    });
+    
+    app.get('/reviews', (req, res) => {
+		reviewCollection.find().toArray((err, items) => {
+			res.send(items);
+		});
+    });
+    
+   app.get('/admin', (req, res) => {
+		console.log(req.query.email);
+		adminCollection.find({ email: req.query.email }).toArray((err, items) => {
+			res.send(items);
+		});
+	});
+
 })
 
 app.listen(port, () => {
